eb3f20ba802b0f1e9248a60de1f6c11fc609c574
apark105 <andypark105@gmail.com>
2018-10-10 10:23:32 -0700

    main js file - andy park

---
+++ main.js
@@ -0,0 +1,54 @@
+$(document).ready(init); 
+//***************************************************************
+//All global variables go down here:
+var player1 = 1;
+var player2 = 2;
+var gameRound = 1; 
+
+
+//***************************************************************
+//All functions that need to be initialized 
+function init(){
+    
+}
+
+//***************************************************************
+function clickHandler(){
+    //if it's game round 1, then it's player 1's turn. call player1.
+    //if it's game round 2, then it's player 2's turn. call player 2. 
+}
+
+//***************************************************************
+function player1(){
+    checkMoveIfValid() 
+    //check all valid moves for player 1, place game piece. 
+    //call display data to flip all game pieces according to player 1's pieces
+    //Change game round 
+    gameRound = 2; 
+}
+
+//***************************************************************
+function player2(){
+
+    gameRound = 1; 
+}
+
+//***************************************************************
+function displayData(){
+
+}
+
+//***************************************************************
+function determineWiner(){
+
+}
+
+//***************************************************************
+function startGameBoard(){
+
+}
+
+//***************************************************************
+function checkMoveIfValid(){
+
+}
